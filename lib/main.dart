9724fcc3a20c318b347ba87f15242651295ae921
PeachBlack-Alba <albatdr@gmail.com>
2021-06-08 14:40:57 +0200

    ui

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Contact Us'),
+        title: Text('Contact App'),
       ),
       body: Column(
         children: [
